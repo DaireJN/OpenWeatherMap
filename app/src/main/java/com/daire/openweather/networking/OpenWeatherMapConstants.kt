fe52eed28f769019199d95debf807f939917952c
DaireJN <DaireOrigin@outlook.com>
2019-11-21 14:33:31 +0000

    Update OpenWeatherMapConstants.kt

--- app/src/main/java/com/daire/openweather/networking/OpenWeatherMapConstants.kt
+++ app/src/main/java/com/daire/openweather/networking/OpenWeatherMapConstants.kt
@@ -4,6 +4,6 @@ class OpenWeatherMapConstants {
 
     companion object {
         const val BASE_URL = "https://api.openweathermap.org/"
-        const val API_KEY = "542ffd081e67f4512b705f89d2a611b2"
+        const val API_KEY = "YOUR_API_KEY"
     }
 }
